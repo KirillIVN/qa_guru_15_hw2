df992632084760c9b1960749e8d3efac16a44c9d
Kirill Ivanenko <kv.ivanenko@mail.ru>
2022-09-16 18:21:38 +0300

    Use random integer

--- src/test/java/topmail/kz/ExampleTest.java
+++ src/test/java/topmail/kz/ExampleTest.java
@@ -3,11 +3,13 @@ package topmail.kz;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class ExampleTest {
 
     @Test
     void exampleTest () {
 
-        Assertions.assertTrue(2>=1);
+        Assertions.assertTrue( new Random().nextInt()>=1);
     }
 }
