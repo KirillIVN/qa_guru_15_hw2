c5ea39ece4b19db0b4c1ec64ebafae1dcf91bf89
Kirill Ivanenko <kv.ivanenko@mail.ru>
2022-09-16 18:27:39 +0300

    new logic

--- src/test/java/topmail/kz/ExampleTest.java
+++ src/test/java/topmail/kz/ExampleTest.java
@@ -6,8 +6,13 @@ import org.junit.jupiter.api.Test;
 public class ExampleTest {
 
     @Test
-    void exampleTest () {
+    void exampleTest1 () {
 
         Assertions.assertTrue(2>=1);
+    }
+
+    void exampleTest2 () {
+
+        Assertions.assertTrue(5>=3);
     }
 }
