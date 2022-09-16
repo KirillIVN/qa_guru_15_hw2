3866e3aacc122290de1f047b2610adf530ddfe40
Kirill Ivanenko <kv.ivanenko@mail.ru>
2022-09-16 16:17:50 +0300

    First commit

---
+++ src/test/java/topmail/kz/ExampleTest.java
@@ -0,0 +1,13 @@
+package topmail.kz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest () {
+
+        Assertions.assertTrue( 2>1 );
+    }
+}
