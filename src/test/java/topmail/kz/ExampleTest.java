0d2d2c4a17a370fe798692a8dbef17af0a526756
Kirill Ivanenko <kv.ivanenko@mail.ru>
2022-09-16 18:43:40 +0300

    exchange values

--- src/test/java/topmail/kz/ExampleTest.java
+++ src/test/java/topmail/kz/ExampleTest.java
@@ -10,11 +10,11 @@ public class ExampleTest {
     @Test
     void exampleTest1 () {
 
-        Assertions.assertTrue( new Random().nextInt()>=5);
+        Assertions.assertTrue( new Random().nextInt()>=8);
     }
 
     void exampleTest2 () {
 
-        Assertions.assertTrue(5>=3);
+        Assertions.assertTrue(6>=3);
     }
 }
